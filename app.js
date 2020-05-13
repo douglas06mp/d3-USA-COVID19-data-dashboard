86a8efd0f38caffde0102bffe36699ba78a9fe73
douglas <douglas06mp@gmail.com>
2020-05-13 11:59:16 +0800

    tooltip on map and pie

--- app.js
+++ app.js
@@ -59,4 +59,62 @@ d3.queue()
       drawPie(data, currentDate, currentDataType);
       drawLine(data, currentDataType, state);
     });
+
+    //TOOLTIP
+    d3.selectAll('svg').on('mousemove touchmove', updateTooltip);
+
+    function updateTooltip() {
+      const tooltip = d3.select('.tooltip');
+      const dataType = d3.select('input:checked').property('value');
+      const date = dateArr[+d3.select('#date').property('value')];
+
+      //CHECK WHICH CHART
+      let target = d3.select(d3.event.target);
+
+      let isState = target.classed('state');
+      let isArc = target.classed('arc');
+      let isLine = target.classed('line');
+
+      //GET DATA BASED ON CHART
+      let data;
+      //Map
+      if (isState) data = target.data()[0].properties;
+      //Line
+      if (isLine) {
+      }
+      //Pie
+      let region = '';
+      let percentage = '';
+      if (isArc) {
+        data = target.data()[0].data;
+
+        region = `<p>Region: ${getRegion(data.state)}</p>`;
+        percentage = `<p>Percentage of total ${dataType}: ${getPercentage(
+          target.data()[0]
+        )}</p>`;
+      }
+
+      //POSITION
+      tooltip
+        .style('opacity', +(isState || isArc || isLine))
+        .style('left', `${d3.event.pageX - tooltip.node().offsetWidth / 2}px`)
+        .style('top', `${d3.event.pageY - tooltip.node().offsetHeight - 10}px`);
+
+      //DISPLAY DATA
+      if (data) {
+        tooltip.html(`
+          ${region}
+          <p>State: ${data.state}</p>
+          <p>Cases: ${data.cases}</p>
+          <p>Deaths: ${data.deaths}</p>
+          ${percentage}
+        `);
+      }
+    }
   });
+
+function getPercentage(d) {
+  let angle = d.endAngle - d.startAngle;
+  let fraction = (angle / (Math.PI * 2)) * 100;
+  return `${fraction.toFixed(2)}%`;
+}
